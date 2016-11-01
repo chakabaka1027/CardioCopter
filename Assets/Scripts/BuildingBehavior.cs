3b958e6511b2277dacc2464c13736f9db0f906d1
chakabaka1027 <alexanderdcha@gmail.com>
2016-11-01 17:04:38 -0400

    added basic control options and the building spawner

---
+++ Assets/Scripts/BuildingBehavior.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuildingBehavior : MonoBehaviour {
+
+	void FixedUpdate(){
+		gameObject.transform.Translate(Vector3.left * 0.05f);
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	Rigidbody2D rb;
+	float thrust = 5;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(Input.GetKeyDown(KeyCode.Space)){
+			rb.AddForce(Vector2.up * thrust, ForceMode2D.Impulse);
+		}
+
+	}
+}
