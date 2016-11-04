ba487a0483362295e3eb487515fd881538146d43
chakabaka1027 <alexanderdcha@gmail.com>
2016-11-03 23:22:41 -0400

    phases have text to introduce them

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class Spawner : MonoBehaviour {
 
@@ -7,17 +8,51 @@ public class Spawner : MonoBehaviour {
 	public GameObject building;
 	public GameObject ring;
 
+	public GameObject phaseIndicator;
+
+	public enum ExerciseStage{warmUp, exercise, coolDown};
+	public ExerciseStage exerciseStage;
+
 	float nextSpawnTime;
+
+
 
 	void Start(){
 //		StartCoroutine(SpawnBuilding());
-		StartCoroutine(SpawnRings());
+		exerciseStage = ExerciseStage.warmUp;
 
+		StartCoroutine(SpawnRings());
+	}
+
+	void Update(){
+		if(Input.GetKeyDown(KeyCode.RightArrow)){
+			exerciseStage = ExerciseStage.exercise;
+			StartCoroutine(SpawnBuilding());
+		}
+	}
+
+	public IEnumerator SpawnRings(){
+
+		phaseIndicator.GetComponent<Text>().text = "Warm Up Phase!";
+
+		yield return new WaitForSeconds(1);
+		phaseIndicator.GetComponent<Animator>().Play("FadeOut");
+
+		while(exerciseStage == ExerciseStage.warmUp){
+			Destroy(Instantiate(ring, new Vector2(14, Random.Range(-3f, 3f)), Quaternion.identity), 15);
+
+			yield return new WaitForSeconds(4f);
+		}
 	}
 
 	public IEnumerator SpawnBuilding(){
 
-		while(true){
+		phaseIndicator.GetComponent<Animator>().Play("Spin");
+		phaseIndicator.GetComponent<Text>().text = "Work Out Phase!";
+		yield return new WaitForSeconds(1);
+		phaseIndicator.GetComponent<Animator>().Play("FadeOut");
+
+		while(exerciseStage == ExerciseStage.exercise){
 			GameObject currentBuilding = Instantiate(building, new Vector2(14, Random.Range(-3f, 1f)), Quaternion.identity) as GameObject;
 			SpawnJewel(currentBuilding.transform.position);
 			Destroy(currentBuilding, 15f);
@@ -30,11 +65,5 @@ public class Spawner : MonoBehaviour {
 		Destroy(Instantiate(jewel, offset + Vector2.up * Random.Range(5f, 7f), Quaternion.identity), 15);
 	}
 
-	public IEnumerator SpawnRings(){
-		while(true){
-			Destroy(Instantiate(ring, new Vector2(14, Random.Range(-3f, 3f)), Quaternion.identity), 15);
 
-			yield return new WaitForSeconds(4f);
-		}
-	}
 }
