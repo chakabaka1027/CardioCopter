f0560e133c76790035be773f69306cb96d3dd10e
chakabaka1027 <alexanderdcha@gmail.com>
2016-11-06 19:24:23 -0500

    added timer

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -4,6 +4,10 @@ using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour {
 
+	public float hours;
+	public float minutes;
+	public float seconds;
+
 	public GameObject phaseIndicator;
 	public Text scoreText;
 	public Text timeText;
@@ -13,12 +17,16 @@ public class UIManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+
 		player = FindObjectOfType<PlayerController>();
 	
 	}
 
 	void Update(){
-		timeText.text = "" + Time.time;
+		hours = (int)(Time.time / 3600f);
+		minutes = (int)(Time.time / 60f);
+		seconds = (int)(Time.time % 60f);
+		timeText.text = hours.ToString("00") + ":" + minutes.ToString("00") + ":" + seconds.ToString("00");
 	}
 	
 	public IEnumerator PhaseIndicatorAnimation(string phaseName){
